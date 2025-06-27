bb8b01d6d9e6e3c30eab03a719eacfe7c5de0226
Lluc Llull <llull99.80@gmail.com>
2025-06-27 12:08:08 +0200

    chore: initial commit - base structure for Angular library

---
+++ projects/ui-lib/src/lib/ui-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ui-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class UiLibComponent {
+
+}
---
+++ projects/ui-lib/src/public-api.ts
@@ -0,0 +1,19 @@
+/*
+ * Public API Surface of ui-lib
+ */
+
+/* COMPONENTS  */
+// export * from './lib/components/core/index';
+// export * from './lib/components/shared/index';
+
+// /*   SERVICES   */
+// export * from './lib/services';
+
+// /*   INTERFACES   */
+// export * from './lib/interfaces/index';
+
+// /*   UTILS   */
+// export * from './lib/utils';
+
+// /*   ENUMS   */
+// export * from './lib/Enum';
