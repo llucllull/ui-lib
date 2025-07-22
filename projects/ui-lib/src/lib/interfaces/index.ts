8cf0b72960c072e812507125cbd5a1602235998e
Lluc Llull <llull99.80@gmail.com>
2025-07-22 18:06:33 +0200

    feat: add imports from new interfaces

--- projects/ui-lib/src/lib/interfaces/index.ts
+++ projects/ui-lib/src/lib/interfaces/index.ts
@@ -2,4 +2,5 @@ export * from './page-config.interface';
 export * from './site-config.interface';
 export * from './ui-lib-button.interface';
 export * from './ui-lib-image.interface';
-
+export * from './ui-lib-lang-item.interface';
+export * from './ui-lib-nav-item.interface';
