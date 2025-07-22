72126a529eff89e0ac8e1d2f45a2b2e9675e0760
Lluc Llull <llull99.80@gmail.com>
2025-07-22 17:56:06 +0200

    feat: lang modal component

--- projects/ui-lib/src/lib/components/core/lang-modal/lang-modal.component.ts
+++ projects/ui-lib/src/lib/components/core/lang-modal/lang-modal.component.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Optional } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UiLibLangItemI } from '../../../interfaces/ui-lib-lang-item.interface';
+import { mapLangModal } from '../../../services/mapper/component-mappers/lang-modal.mapper';
+
+interface LangModalData {
+  langs: any[];
+  currentLang: string;
+}
 
 @Component({
   selector: 'lib-lang-modal',
@@ -10,5 +18,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LangModalComponent {
+  langs!: UiLibLangItemI[];
 
+  constructor(
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: LangModalData,
+    @Optional() public dialogRef?: MatDialogRef<LangModalComponent>
+  ) {
+    this.langs = mapLangModal(data.langs, data.currentLang);
+  }
+
+  selectLang(lang: UiLibLangItemI) {
+    this.dialogRef?.close(lang);
+  }
 }
