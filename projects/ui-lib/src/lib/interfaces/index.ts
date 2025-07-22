93c6d5d048d0e1c7874b9308646bd4893d1eaba3
Lluc Llull <llull99.80@gmail.com>
2025-07-22 18:08:44 +0200

    feat: fix import

--- projects/ui-lib/src/lib/interfaces/index.ts
+++ projects/ui-lib/src/lib/interfaces/index.ts
@@ -3,4 +3,4 @@ export * from './site-config.interface';
 export * from './ui-lib-button.interface';
 export * from './ui-lib-image.interface';
 export * from './ui-lib-lang-item.interface';
-export * from './ui-lib-nav-item.interface';
+export * from './ui-lib-nav-items.interface';
