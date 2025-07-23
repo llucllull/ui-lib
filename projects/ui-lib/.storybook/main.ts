5f411490bea5ef5d087e09f6fb759aaa9d2c9079
Lluc Llull <llull99.80@gmail.com>
2025-07-23 19:29:52 +0200

    feat: fiox storybook

--- projects/ui-lib/.storybook/main.ts
+++ projects/ui-lib/.storybook/main.ts
@@ -1,18 +1,19 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions"
   ],
-  "framework": {
-    "name": "@storybook/angular",
-    "options": {}
+  framework: {
+    name: "@storybook/angular",
+    options: {}
   }
 };
+
 export default config;
