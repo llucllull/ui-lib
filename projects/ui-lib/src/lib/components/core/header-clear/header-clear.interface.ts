c112aa569f90b03519e9eba97183b29ce0d412dc
Lluc Llull <llull99.80@gmail.com>
2025-07-28 17:00:19 +0200

    feat: add headerclear interface

--- projects/ui-lib/src/lib/components/core/header-clear/header-clear.interface.ts
+++ projects/ui-lib/src/lib/components/core/header-clear/header-clear.interface.ts
@@ -1,3 +1,8 @@
+import { UiLibImageI } from "../../../interfaces/ui-lib-image.interface";
+import { UiLibNavItemsI } from "../../../interfaces/ui-lib-nav-items.interface";
+
 export interface HeaderClearI {
-  
+  logo?: UiLibImageI;
+  lang?: string;
+  navItems?: UiLibNavItemsI[];
 }
