2d9403671599a2dba80b24ef6cb66ea079add1ce
Lluc Llull <llull99.80@gmail.com>
2025-07-29 12:25:18 +0200

    feat: add mapper for section-intro

--- projects/ui-lib/src/lib/services/mapper/component-mappers/index.ts
+++ projects/ui-lib/src/lib/services/mapper/component-mappers/index.ts
@@ -2,10 +2,12 @@ import { mapHeaderClear } from './header-clear.mapper';
 import { mapHeroSection } from './hero-section.mapper';
 import { mapLangModal } from './lang-modal.mapper';
 import { mapNavModal } from './nav-modal.mapper';
+import { mapSectionIntro } from './section-intro.mapper';
 
 export const componentMappers: Record<string, (props: any) => any> = {
     'hero-section': mapHeroSection,
     'header-clear': mapHeaderClear,
     'nav-modal': mapNavModal,
     'lang-modal': mapLangModal,
+    'section-intro': mapSectionIntro,
 };
