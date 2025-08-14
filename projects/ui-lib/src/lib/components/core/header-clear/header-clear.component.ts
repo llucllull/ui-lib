3e19a1f4f0b1b8be9fdd28d075707e406645b0ae
Lluc Llull <llull99.80@gmail.com>
2025-08-14 12:46:44 +0200

    feat: implement theme toggle functionality in header components

--- projects/ui-lib/src/lib/components/core/header-clear/header-clear.component.ts
+++ projects/ui-lib/src/lib/components/core/header-clear/header-clear.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { LangModalComponent } from "../lang-modal";
-import { NavModalComponent } from "../nav-modal";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LinkTypeDirective } from '../../../directives';
+import { UiLibButtonI } from '../../../interfaces/ui-lib-button.interface';
 import { UiLibImageI } from '../../../interfaces/ui-lib-image.interface';
 import { UiLibNavItemsI } from '../../../interfaces/ui-lib-nav-items.interface';
-import { UiLibButtonI } from '../../../interfaces/ui-lib-button.interface';
-import { LinkTypeDirective } from '../../../directives';
+import { LangModalComponent } from "../lang-modal";
+import { NavModalComponent } from "../nav-modal";
+import { Theme, ThemeService } from '../../../services/theme';
 
 @Component({
   selector: 'lib-header-clear',
@@ -15,8 +17,10 @@ import { LinkTypeDirective } from '../../../directives';
   styleUrl: './header-clear.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderClearComponent {
+export class HeaderClearComponent implements OnInit, OnDestroy {
   isMenuOpen = false;
+  currentTheme: Theme = 'light';
+  private themeSubscription?: Subscription;
   
   @Input() logo?: UiLibImageI;
   @Input() lang?: string;
@@ -26,15 +30,32 @@ export class HeaderClearComponent {
   @Output() langModal = new EventEmitter<void>();
   @Output() theme = new EventEmitter<void>();
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeSubscription = this.themeService.currentTheme$.subscribe(theme => {
+      this.currentTheme = theme;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription?.unsubscribe();
+  }
+
   openLanguagesModal(): void {
     this.langModal.emit();
   }
 
   toggleTheme(): void {
+    this.themeService.toggleTheme();
     this.theme.emit();
   }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  getThemeIcon(): string {
+    return this.currentTheme === 'light' ? '🌙' : '☀️';
   }
 }
--- projects/ui-lib/src/lib/components/core/header-mobile/header-mobile.component.ts
+++ projects/ui-lib/src/lib/components/core/header-mobile/header-mobile.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { UiLibNavItemsI } from '../../../interfaces/ui-lib-nav-items.interface';
-import { LangModalComponent } from '../lang-modal/lang-modal.component';
-import { NavModalComponent } from '../nav-modal/nav-modal.component';
-import { UiLibImageI } from '../../../interfaces/ui-lib-image.interface';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LinkTypeDirective } from '../../../directives/link-type.directive';
 import { UiLibButtonI } from '../../../interfaces/ui-lib-button.interface';
+import { UiLibImageI } from '../../../interfaces/ui-lib-image.interface';
+import { UiLibNavItemsI } from '../../../interfaces/ui-lib-nav-items.interface';
+import { Theme, ThemeService } from '../../../services/theme';
+import { LangModalComponent } from '../lang-modal/lang-modal.component';
+import { NavModalComponent } from '../nav-modal/nav-modal.component';
 
 @Component({
   selector: 'lib-header-mobile',
@@ -15,8 +17,10 @@ import { UiLibButtonI } from '../../../interfaces/ui-lib-button.interface';
   styleUrl: './header-mobile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderMobileComponent {
+export class HeaderMobileComponent implements OnInit, OnDestroy {
   isMenuOpen = false;
+  currentTheme: Theme = 'light';
+  private themeSubscription?: Subscription;
 
   @Input() logo?: UiLibImageI;
   @Input() lang?: string;
@@ -26,15 +30,32 @@ export class HeaderMobileComponent {
   @Output() langModal = new EventEmitter<void>();
   @Output() theme = new EventEmitter<void>();
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeSubscription = this.themeService.currentTheme$.subscribe(theme => {
+      this.currentTheme = theme;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription?.unsubscribe();
+  }
+
   openLanguagesModal(): void {
     this.langModal.emit();
   }
 
   toggleTheme(): void {
+    this.themeService.toggleTheme();
     this.theme.emit();
   }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  getThemeIcon(): string {
+    return this.currentTheme === 'light' ? '🌙' : '☀️';
   }
 }
