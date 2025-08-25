fd95f94d8612b869aa42b37c7452f1c694e6749a
Lluc Llull <llull99.80@gmail.com>
2025-08-25 18:38:57 +0200

    feat: fix social-footer component

--- projects/ui-lib/src/lib/components/core/social-footer/social-footer.component.ts
+++ projects/ui-lib/src/lib/components/core/social-footer/social-footer.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { UiLibButtonI, UiLibImageI } from '../../../interfaces';
 import { LinkTypeDirective, SwiperDirective } from '../../../directives';
+import { UiIconComponent } from '../../shared';
 
 @Component({
   selector: 'lib-social-footer',
   standalone: true,
-  imports: [CommonModule, LinkTypeDirective, SwiperDirective],
+  imports: [CommonModule, LinkTypeDirective, SwiperDirective, UiIconComponent],
   templateUrl: './social-footer.component.html',
   styleUrl: './social-footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
