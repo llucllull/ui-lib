6e709b497a366cec767a9bd36d57f5ddcfcaa796
Lluc Llull <llull99.80@gmail.com>
2025-08-26 19:02:18 +0200

    feat: add new interface uilibaddress

--- projects/ui-lib/src/lib/interfaces/index.ts
+++ projects/ui-lib/src/lib/interfaces/index.ts
@@ -4,3 +4,4 @@ export * from './ui-lib-button.interface';
 export * from './ui-lib-image.interface';
 export * from './ui-lib-lang-item.interface';
 export * from './ui-lib-nav-items.interface';
+export * from './ui-lib-address.interface';
