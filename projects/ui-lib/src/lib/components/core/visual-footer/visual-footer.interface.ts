2e8eb7a73ada8b904503f83773b6c3a84f3c76a3
Lluc Llull <llull99.80@gmail.com>
2025-08-26 19:12:55 +0200

    feat: visual-component

---
+++ projects/ui-lib/src/lib/components/core/visual-footer/visual-footer.interface.ts
@@ -0,0 +1,12 @@
+import { UiLibAddressI, UiLibButtonI, UiLibImageI } from "../../../interfaces";
+
+export interface VisualFooterI {
+  contactTitle?: string;
+  contactPhone?: UiLibButtonI;
+  contactEmail?: UiLibButtonI;
+  addressTitle?: string;
+  address?: UiLibAddressI;
+  socialsTitle?: string;
+  socials?: UiLibButtonI[];
+  image?: UiLibImageI;
+}
